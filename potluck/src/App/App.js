2edf21c6c3681160a326de0d7bce4b96f19e27a5
bbalconi <bbalconi@gmail.com>
2017-10-30 15:07:24 -0600

    hashrouter2

--- potluck/src/App/App.js
+++ potluck/src/App/App.js
@@ -6,10 +6,9 @@ import Login from "../Login/Login";
 import Navvy from "../Nav/Nav.js";
 import House from "../CreateHouse/CreateHouse.js";
 import JoinHouse from "../JoinHouse/JoinHouse.js";
-import { Router, hashHistory } from 'react-router';
 import Timer from "../timer.js";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Route,
 } from 'react-router-dom';
 var axios = require('axios');
@@ -62,7 +61,7 @@ class App extends Component {
     }
         if(sessionStorage.name != null){
             return (
-                <Router history={ hashHistory }>
+                <Router>
                 <div className='bg'>
                     <Route path='/' render={()=><Navvy logOut={this.logOut} currentUser={this.state.currentUser}/>} />
                     <Route path='/Login' render={() => <Login submitLogin={this.submitLogin} />}/>
@@ -77,7 +76,7 @@ class App extends Component {
             )
         }else{
             return (
-                <Router history={ hashHistory }>
+                <Router>
                 <div className='bg'>
                     <Route path='/' render={()=><Navvy logOut={this.logOut} currentUser={this.state.currentUser}/>} />
                     <Route path='/Login' render={() => <Login submitLogin={this.submitLogin} />}/>
